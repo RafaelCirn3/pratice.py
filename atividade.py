ee1bc6161bce04566927febdf2b2d08f1157f84d
Rafael Cirne <125037856+RafaelCirn3@users.noreply.github.com>
2023-09-20 14:03:50 -0300

    Delete atividade.py

--- atividade.py
+++
@@ -1,8 +0,0 @@
-nome = input('digite seu nome: ')
-salario = float(input('digite seu salário:R$'))
-if salario >= 1200 and salario < 2400: 
-    print(nome ,'você recebeu um aumento de 15%, recebendo agora :R$',salario*1.15)
-elif salario >=2400:
-    print('nome,você recebeu um aumento de 7%, recebendo agora :R$',salario*1.07)
-else: 
-    print('nenhum aumento para', nome)
